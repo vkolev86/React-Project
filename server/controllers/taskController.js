e1ac1bb84edef6d8a1f45b04b2af2e4ef2c83704
Vasil Kolev <v.s.kolev@gmail.com>
2023-12-06 20:26:31 +0200

    Add back-end (server)

---
+++ server/controllers/taskController.js
@@ -0,0 +1,85 @@
+const User = require('../model/user.model');
+const Task = require('../model/task.model');
+
+const addTask = async (req, res) => {
+	const { task, id } = req.body;
+
+	try {
+		if (!task) return res.status(400).send('please enter the task');
+		if (task.length < 5) res.status(400).send('add minimum 5 char');
+		const taskDetail = await new Task({
+			task,
+			cretedBy: id,
+		});
+		await taskDetail.save();
+		return res.status(200).send(taskDetail);
+	} catch (error) {
+		return res.status(400).send('task addition failed');
+	}
+};
+
+const getAllTasks = async (req, res) => {
+	try {
+		let tasklist = await Task.find();
+		return res.status(200).send(tasklist);
+	} catch (error) {
+		return res.status(400).send(error);
+	}
+};
+
+const editTask = async (req, res) => {};
+
+const statusChange = async (req, res) => {
+	const { id, string } = req.body;
+
+	try {
+		let task = await Task.findById({ _id: id });
+		if (string === 'right') {
+			if (task.status === 'new') {
+				task.status = 'todo';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'todo') {
+				task.status = 'doing';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'doing') {
+				task.status = 'done';
+				task.save();
+				return res.send(task);
+			}
+		} else {
+			if (task.status === 'done') {
+				task.status = 'doing';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'doing') {
+				task.status = 'todo';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'todo') {
+				task.status = 'new';
+				task.save();
+				return res.send(task);
+			}
+		}
+	} catch (error) {}
+};
+
+const deleteTask = async (req, res) => {
+	const { id } = req.params;
+	try {
+		let response = await Task.findByIdAndDelete(id);
+		return res.status(200).send(response);
+	} catch (error) {
+		return res.status(400).send('deleteFailed');
+	}
+};
+
+module.exports = {
+	addTask,
+	getAllTasks,
+	editTask,
+	statusChange,
+	deleteTask,
+};
---
+++ server/model/task.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema(
+	{
+		task: { type: String },
+		status: {
+			type: String,
+			enum: ['new', 'todo', 'doing', 'done'],
+			default: 'new',
+		},
+		cretedBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	},
+	{ timestamp: true }
+);
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
